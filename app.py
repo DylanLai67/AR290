977fe0d19f9d207dcdd4f981ddc8a26525994815
Dylan Lai <dylanlai@crc-dot1x-nat-10-239-75-38.bu.edu>
2022-11-28 15:21:56 -0500

    Route Bullets made

--- app.py
+++ app.py
@@ -10,7 +10,22 @@ def index():
 def aboutpompeii():
     return render_template('aboutpompeii.html')
 
+@app.route('/newdiscovery')
+def newdiscovery():
+    return render_template('newdiscovery.html')
 
+@app.route('/humanenv')
+def humanenv():
+    return render_template('humanenv.html')
 
+@app.route('/archadvance')
+def archadvance():
+    return render_template('archadvance.html')
 
+@app.route('/modernday')
+def modernday():
+    return render_template('modernday.html')
 
+@app.route('/sources')
+def sources():
+    return render_template('sources.html')
