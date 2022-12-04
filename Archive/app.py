e5daf96fca138986d95758af28c4ad3eecab1f33
DylanLai67 <dylanl67@bu.edu>
2022-12-04 12:04:50 -0500

    Final

---
+++ Archive/app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/aboutpompeii')
+def aboutpompeii():
+    return render_template('aboutpompeii.html')
+
+@app.route('/newdiscovery')
+def newdiscovery():
+    return render_template('newdiscovery.html')
+
+@app.route('/humanenv')
+def humanenv():
+    return render_template('humanenv.html')
+
+@app.route('/archadvance-modernday')
+def archadvancemodernday():
+    return render_template('archadvance-modernday.html')
+
+
+@app.route('/sources')
+def sources():
+    return render_template('sources.html')
+
+if __name__=="__main__":
+    app.run(debug=False,host='0.0.0.0')
+    
